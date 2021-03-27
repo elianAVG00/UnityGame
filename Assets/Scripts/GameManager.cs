3813419537fdcb46723df463e77d382bae2f61f5
Elian <anunciatres.m@gmail.com>
2021-03-27 04:13:47 -0300

    Add gameManager and time countdown

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //tener acceso desde otro codigo sin tener q instanciarlo, al ser static la variable existe en memoria hasta q se cierre la app
+    public static GameManager instance;
+    [SerializeField]int time = 30;
+
+    //aplico singleton para instanciar el objeto una unica vez
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CountdownRoutine());
+    }
+
+    IEnumerator CountdownRoutine()
+    {
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+            time--;
+        }
+    }
+}
