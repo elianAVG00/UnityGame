84664e576c77524a8e46d2f2f4abcf8060e5cd83
Elian <anunciatres.m@gmail.com>
2021-03-27 02:53:22 -0300

    Add aim to player and config camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
     float h;
     float v;
     Vector3 moveDirection;
-    float speed = 3;
+    //Nos permite modificarlo desde Unity
+    [SerializeField] float speed = 3;
+    [SerializeField] Transform aim;
+    [SerializeField] Camera cam;
+    Vector2 facingDirection;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,13 @@ public class Player : MonoBehaviour
         moveDirection.y = v;
 
         //transform almacena los datos del GameObject actual en este caso de Player, deltaTime es el Time de la pc(al multiplicar por el movimiento de player normalizamos su movimiento al de la pc usada)
-        transform.position += moveDirection * Time.deltaTime;
+        transform.position += moveDirection * Time.deltaTime * speed;
+
+        //Movimiento de la mira:
+        //Asigno a fDirection -> a la posicion del mouse ( con cam lo traduzco de posicion de pantalla a posicion de mundo/juego) y le resto lapos del player para q la mira se vaya moviendo segun el player, no solo segun el mouse
+        facingDirection = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        aim.position = transform.position + (Vector3)facingDirection.normalized;
+
     }
 }
